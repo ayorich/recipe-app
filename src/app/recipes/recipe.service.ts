46770322e834dee923a5f676c25593c91c458731
ayorich <kayodele17@yahoo.com>
2021-08-09 05:41:12 +0100

    auth

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -7,21 +7,26 @@ import { Recipe } from './recipe.model';
 @Injectable()
 export class RecipeService {
   recipesChaged = new Subject<Recipe[]>();
-  private recipes: Recipe[] = [
-    new Recipe(
-      'Tasty recipe',
-      'simply a test',
-      'https://i1.wp.com/www.eatthis.com/wp-content/uploads/2020/02/air-fryer-falafel.jpg?resize=640%2C360&ssl=1',
-      [new Ingredient('Meat', 1), new Ingredient('Meat', 19)]
-    ),
-    new Recipe(
-      'Burger recipe',
-      'simply a test',
-      'https://i1.wp.com/www.eatthis.com/wp-content/uploads/2020/02/air-fryer-falafel.jpg?resize=640%2C360&ssl=1',
-      [new Ingredient('Buns', 2), new Ingredient('Meat', 6)]
-    ),
-  ];
+  // private recipes: Recipe[] = [
+  //   new Recipe(
+  //     'Tasty recipe',
+  //     'simply a test',
+  //     'https://i1.wp.com/www.eatthis.com/wp-content/uploads/2020/02/air-fryer-falafel.jpg?resize=640%2C360&ssl=1',
+  //     [new Ingredient('Meat', 1), new Ingredient('Meat', 19)]
+  //   ),
+  //   new Recipe(
+  //     'Burger recipe',
+  //     'simply a test',
+  //     'https://i1.wp.com/www.eatthis.com/wp-content/uploads/2020/02/air-fryer-falafel.jpg?resize=640%2C360&ssl=1',
+  //     [new Ingredient('Buns', 2), new Ingredient('Meat', 6)]
+  //   ),
+  // ];
+  private recipes: Recipe[] = [];
   constructor(private slService: ShoppingListService) {}
+  setRecipes(recipes: Recipe[]) {
+    this.recipes = recipes;
+    this.recipesChaged.next(this.recipes.slice());
+  }
   getRecipes() {
     return this.recipes.slice();
   }
