8d3cc5a8baa8d782338b9351a586da8204e983d0
ayorich <kayodele17@yahoo.com>
2021-09-03 04:54:22 +0100

    recipe-reducer

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -2,8 +2,9 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { Recipe } from '../recipe.model';
-import { RecipeService } from '../recipe.service';
-
+import * as fromApp from '../../store/app.reducer';
+import { Store } from '@ngrx/store';
+import { map } from 'rxjs/operators';
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
@@ -13,18 +14,18 @@ export class RecipeListComponent implements OnInit, OnDestroy {
   recipes: Recipe[];
   subscription: Subscription;
   constructor(
-    private recipeService: RecipeService,
     private router: Router,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private store: Store<fromApp.AppState>
   ) {}
 
   ngOnInit() {
-    this.subscription = this.recipeService.recipesChaged.subscribe(
-      (recipes: Recipe[]) => {
+    this.subscription = this.store
+      .select('recipes')
+      .pipe(map((recipesState) => recipesState.recipes))
+      .subscribe((recipes: Recipe[]) => {
         this.recipes = recipes;
-      }
-    );
-    this.recipes = this.recipeService.getRecipes();
+      });
   }
 
   onNewRecipe() {
---
+++ src/app/recipes/store/recipe.reducer.ts
@@ -0,0 +1,23 @@
+import { Recipe } from '../recipe.model';
+import * as RecipesActions from './recipe.action';
+export interface State {
+  recipes: Recipe[];
+}
+
+const initialState: State = {
+  recipes: [],
+};
+export function recipeReducer(
+  state = initialState,
+  action: RecipesActions.RecipsActions
+) {
+  switch (action.type) {
+    case RecipesActions.SET_RECIPES:
+      return {
+        ...state,
+        recipes: [...action.payload],
+      };
+    default:
+      return state;
+  }
+}
